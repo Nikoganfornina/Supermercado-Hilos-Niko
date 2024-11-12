7749b2dab2a0fee67564e4f4fdfdbf3e7b047790
Nicolás Ganfornina García <145467889+Nikoganfornina@users.noreply.github.com>
2024-11-12 16:25:15 +0100

    Add files via upload

---
+++ SupermercadoNiko/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //This is the Thread exercise about the supermarket
+
+
+    }
+}
